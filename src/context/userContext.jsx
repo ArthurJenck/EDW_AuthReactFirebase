3dd924223b9a333356b4f308655870e62d716408
Arthur Jenck <arthurjenckdev@gmail.com>
2025-02-21 11:55:26 +0100

    fix: adapted env variables for vite

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,7 +1,16 @@
 import { useState } from "react"
 import { UserContext } from "./createContext"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../firebase-config"
 
 export const UserContextProvider = (props) => {
+  const [currentUser, setCurrentUser] = useState()
+  const [loadingData, setLoadingData] = useState(true)
+
+  const signUp = (email, pwd) =>
+    createUserWithEmailAndPassword(auth, email, pwd)
+
+  // MODALS
   const [modalState, setModalState] = useState({
     signUpModal: false,
     logInModal: false,
